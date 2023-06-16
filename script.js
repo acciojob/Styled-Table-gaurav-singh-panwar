dfff4813057f59f42cf6a9c81e2aa81f2484f8a5
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-06-16 20:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Your code here.
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 20px;
+}
+
+h1 {
+    text-align: center;
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+thead {
+    background-color: #f5f5f5;
+}
+
+th,
+td {
+    padding: 10px;
+    text-align: center;
+}
+
+th {
+    font-weight: bold;
+    border-bottom: 1px solid #ddd;
+}
+
+tbody tr:nth-child(even) {
+    background-color: #f9f9f9;
+}
